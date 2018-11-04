b990e4b05465eab502a62bb6750bca81bbae821f
sgoutam <sanket.ishu.09@gmail.com>
2018-11-03 21:30:18 -0400

    cleaning clingo output

---
+++ test.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+arr = []
+
+
+outfile = open("input.txt", "w")
+
+with open("trial.txt", "r") as f:
+    for line in f:
+        if line.startswith("at("):
+            words = line.strip('\n').split(" ")
+            for word in words:
+                x = word.rstrip("at(").rstrip(")").split(",")[2]
+                outfile.write(x)
+                outfile.write('\n')
+
+outfile.close()
